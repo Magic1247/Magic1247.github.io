6f79a983e4746eb6c02554c8a3bb68394c821ba3
loki.zuo <zyhttx@Gmail.com>
2024-10-28 10:38:30 +0800

    Site updated: 2024-10-28 10:38:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/hello-world/"];function toRandomPost(){
+var posts=["2024/10/28/hello-world/","2022/02/23/python_share/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
