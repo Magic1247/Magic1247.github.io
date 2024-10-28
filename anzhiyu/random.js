fa5ab2f97be938af45a161ef11241c0cb060b62d
loki.zuo <zyhttx@Gmail.com>
2024-10-28 14:29:58 +0800

    Site updated: 2024-10-28 14:29:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/Go语言概述/","2022/02/23/Python 技巧：提升代码效率与简洁度/","2024/10/28/Go语言环境安装/","2024/10/28/从0掌握Python接口自动化测试/","2024/10/28/使用Python-pytest-requests-allure-yaml搭建数据驱动的接口自动化框架/"];function toRandomPost(){
+var posts=["2024/10/28/Go-HelloWorld/","2024/10/28/GoLang完整学习路线行/","2024/10/28/Go-编译与运行/","2024/10/28/Go语言概述/","2024/10/28/Go语言环境安装/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
