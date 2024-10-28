e8f39c158bc768e854e1d49a1864c3f55e024a7b
loki.zuo <zyhttx@Gmail.com>
2024-10-28 12:29:44 +0800

    Site updated: 2024-10-28 12:29:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/02/23/Python 技巧：提升代码效率与简洁度/","2024/10/28/从0掌握Python接口自动化测试/","2024/10/28/hello-world/"];function toRandomPost(){
+var posts=["2022/02/23/Python 技巧：提升代码效率与简洁度/","2024/10/28/从0掌握Python接口自动化测试/","2024/10/28/hello-world/","2024/10/28/使用Python-pytest-requests-allure-yaml搭建数据驱动的接口自动化框架/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
