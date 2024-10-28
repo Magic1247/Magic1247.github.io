550c7b3086446fd7851664a73de4f9cbe62b8612
loki.zuo <zyhttx@Gmail.com>
2024-10-28 11:05:00 +0800

    Site updated: 2024-10-28 11:05:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/hello-world/","2022/02/23/Python 技巧：提升代码效率与简洁度/"];function toRandomPost(){
+var posts=["2022/02/23/Python 技巧：提升代码效率与简洁度/","2024/10/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
