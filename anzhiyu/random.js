02005c045cde43c6c923c90f4c56b9cfd32dce61
loki.zuo <zyhttx@Gmail.com>
2024-11-01 10:38:13 +0800

    Site updated: 2024-11-01 10:38:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/Go-HelloWorld/","2024/10/28/Go-编译与运行/","2024/10/28/GoLang完整学习路线行/","2024/10/28/Go语言概述/","2024/10/28/Go语言环境安装/"];function toRandomPost(){
+var posts=["2024/10/28/Go-HelloWorld/","2024/10/30/Go-函数定义与使用/","2024/10/30/Go-变量、常量与数据类型/","2024/10/30/Go-控制结构/","2024/10/28/Go-编译与运行/","2024/11/01/Go-数组与切片/","2024/10/28/GoLang完整学习路线行/","2024/10/28/Go语言概述/","2024/10/28/Go语言环境安装/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
